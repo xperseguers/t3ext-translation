7455f82dd49c7ab4d23d385fd53caf4abc9addd3
Xavier Perseguers <xavier@causal.ch>
2017-02-04 16:37:50 +0100

    [TASK] Adapt headers for use within TYPO3

--- Classes/I18n/Exception/InvalidArgumentException.php
+++ Classes/I18n/Exception/InvalidArgumentException.php
@@ -1,15 +1,20 @@
 <?php
+/*
+ * This file is part of the TYPO3 CMS project and originates from
+ * the Neos.Flow package from the Neos Project - www.neos.io.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with TYPO3 source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 namespace Neos\Flow\I18n\Exception;
 
-/*
- * This file is part of the Neos.Flow package.
- *
- * (c) Contributors of the Neos Project - www.neos.io
- *
- * This package is Open Source Software. For the full copyright and license
- * information, please view the LICENSE file which was distributed with this
- * source code.
- */
 use Neos\Flow\I18n;
 
 /**
--- Classes/I18n/LocaleTypeConverter.php
+++ Classes/I18n/LocaleTypeConverter.php
@@ -1,15 +1,19 @@
 <?php
-namespace Neos\Flow\I18n;
-
 /*
- * This file is part of the Neos.Flow package.
+ * This file is part of the TYPO3 CMS project and originates from
+ * the Neos.Flow package from the Neos Project - www.neos.io.
  *
- * (c) Contributors of the Neos Project - www.neos.io
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- * This package is Open Source Software. For the full copyright and license
- * information, please view the LICENSE file which was distributed with this
- * source code.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with TYPO3 source code.
+ *
+ * The TYPO3 project - inspiring people to share!
  */
+
+namespace Neos\Flow\I18n;
 
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Property\PropertyMappingConfigurationInterface;
